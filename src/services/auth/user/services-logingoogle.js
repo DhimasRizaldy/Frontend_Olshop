14a50e12b93c81224c2896db45bddeb14ab3b77e
DhimasRizaldy <dtekchannel02@gmail.com>
2024-09-14 15:35:29 +0700

    feat:login google

---
+++ src/services/auth/user/services-logingoogle.js
@@ -0,0 +1,26 @@
+import { toast } from 'react-toastify';
+import http from '../../../utils/constants/http';
+import { API_ENDPOINT } from '../../../utils/constants/endpoint';
+import Swal from 'sweetalert2';
+
+// handle login with google
+export const loginWithGoogle = async (userData, setIsLoading) => {
+  setIsLoading(true);
+  try {
+    const response = await http.post(API_ENDPOINT.USER_LOGIN_GOOGLE, userData);
+    return response.data;
+  } catch (error) {
+    const errorMessage =
+      error.response?.data?.message || 'Error login with Google';
+    console.error(
+      'Error login with Google:',
+      error.response?.data || errorMessage,
+    );
+    toast.error(errorMessage);
+    throw new Error(errorMessage);
+  } finally {
+    setIsLoading(false);
+  }
+};
+
+export default loginWithGoogle;
