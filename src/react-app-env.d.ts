fe5461193e91e3b96453f54747197c5ce8990f66
DhimasRizaldy <dtekchannel02@gmail.com>
2024-09-15 12:05:05 +0700

    feat:bug fix login google oauth

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -2,3 +2,15 @@ declare module '*.png';
 declare module '*.svg';
 declare module '*.jpeg';
 declare module '*.jpg';
+
+
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_GOOGLE_CLIENT_ID: string;
+  // Add other environment variables here if needed
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
