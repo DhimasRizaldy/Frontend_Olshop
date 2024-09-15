78e85e4fd499e1263a921336a77a014fb48d7dd8
DhimasRizaldy <dtekchannel02@gmail.com>
2024-09-15 15:24:49 +0700

    feat:fix test1

--- src/services/auth/user/services-logingoogle.js
+++ src/services/auth/user/services-logingoogle.js
@@ -5,21 +5,22 @@ import { toast } from 'react-toastify';
 // handle Google login action
 export const loginWithGoogle = async (accessToken, navigate) => {
   try {
+    // Konversi data menjadi JSON string
     const data = JSON.stringify({
       access_token: accessToken,
     });
 
-    const config = {
-      method: 'post',
-      maxBodyLength: Infinity,
-      url: 'https://backend-olshop.vercel.app/api/v1/auth/google',
-      headers: {
-        'Content-Type': 'application/json',
+    // Lakukan request dengan axios
+    const response = await axios.post(
+      'https://backend-olshop.vercel.app/api/v1/auth/google',
+      data,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
       },
-      data: data,
-    };
+    );
 
-    const response = await axios.request(config);
     const { token } = response.data;
 
     // Simpan token di dalam Cookie
@@ -28,6 +29,10 @@ export const loginWithGoogle = async (accessToken, navigate) => {
     toast.success('Login successful');
     navigate('/'); // Redirect setelah login sukses
   } catch (error) {
+    // Cetak error ke console untuk debugging
+    console.error('Login failed', error);
+
+    // Pesan kesalahan untuk pengguna
     toast.error('Login failed');
   }
 };
